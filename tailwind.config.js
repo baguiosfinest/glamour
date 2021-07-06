3e9098239737b162b208e5023fb8bd754de49e04
Sonny Tambiac <baguiosfinest007@gmail.com>
2021-07-06 19:22:56 +1000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    // './src/**/*.html',
+    // './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#081624'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
